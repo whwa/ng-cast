04acd14e76eff0cf12b789b8fd6c16317db68a2a
Hack Reactor Student <student@hackreactor.com>
2017-09-12 12:08:22 -0700

    Impement YouTubeAPI request and show initially on page

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,18 @@
 angular.module('video-player')
 
 .component('app', {
-  controller: function() {
+  controller: function($scope, youTube) {
     this.videos = exampleVideoData;
     this.index = 0;
-    this.video = exampleVideoData[this.index];
+    this.video = this.videos[this.index];
     this.selectVideo = (index) => {
       this.index = index;
-      this.video = exampleVideoData[this.index];
+      this.video = this.videos[this.index];
     };
+    youTube.search((data) => {
+      this.videos = data;
+      this.video = this.videos[0];
+    });
   },
   templateUrl: '/src/templates/app.html'
 });
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,22 +1,24 @@
 angular.module('video-player')
-.service('youTube', function(cb) {
-  $http({
-    method: 'GET',
-    url: 'https://www.googleapis.com/youtube/v3/search',
-    params: {
-      q: 'tiny animals eating',
-      maxResults: 5,
-      part: 'snippet',
-      type: 'video',
-      videoEmbeddable: 'true',
-      key: YOUTUBE_API_KEY
-    }
+.service('youTube', function($http) {
+  this.search = function(cb) {
     
+    $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      params: {
+        q: 'tiny animals eating',
+        maxResults: 5,
+        part: 'snippet',
+        type: 'video',
+        videoEmbeddable: 'true',
+        key: YOUTUBE_API_KEY
+      }
+      
+    }).then(function successCallback(response) {
+      cb(response.data.items);
+    }, function error(response) {
+      console.log('fail', response);
+    });
     
-  }).then(function successCallback(response) {
-    console.log('success', response);
-    //call cb
-  }, function error(response) {
-    console.log('fail');
-  });
+  };
 });
